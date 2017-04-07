a60f65570f329de7df3cd499af3bd48d370a9d0c
hao.guo <hao.guo@rci.rogers.com>
2017-04-07 15:55:33 -0400

    working

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,25 @@
 'use strict';
 
 var app = angular.module('sudokuApp', []);
-app.directive('mySudoku', function() {
+app
+.directive('mySudokubox', function() {
     return {
-        templateUrl: "/js/template/mySudoku.html"
+        terminal: true,
+        priority: 10,
+        templateUrl: "js/template/mySudokuBox.html"
+    }
+})
+.directive('mySudoku', function() {
+    return {
+        require: 'mySudokubox',
+        transclude: true,
+        templateUrl: "/js/template/mySudoku.html",
+        controller: function($scope) {
+            $scope.data = [
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]
+            ]
+        }
     };
 });
