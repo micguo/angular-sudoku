aac377da74d0f3e58754106b182878667c28fb14
Hao Guo <micguo2000@gmail.com>
2017-04-08 17:19:07 -0400

    Display complete

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,85 @@
 'use strict';
 
 var app = angular.module('sudokuApp', []);
-app
-.directive('mySudokubox', function() {
+app.controller('MainCtrl', function($scope) {
+    $scope.sodukuData = [
+        [
+            [
+                [1,2,3],
+                [4,5,6],
+                [7,8,9]
+            ],
+            [
+                [1,2,3],
+                [4,5,6],
+                [7,8,9]
+            ],
+            [
+                [1,2,3],
+                [4,5,6],
+                [7,8,9]
+            ]
+        ],
+        [
+            [
+                [1,2,3],
+                [4,5,6],
+                [7,8,9]
+            ],
+            [
+                [1,2,3],
+                [4,5,6],
+                [7,8,9]
+            ],
+            [
+                [1,2,3],
+                [4,5,6],
+                [7,8,9]
+            ]
+        ],
+        [
+            [
+                [1,2,3],
+                [4,5,6],
+                [7,8,9]
+            ],
+            [
+                [1,2,3],
+                [4,5,6],
+                [7,8,9]
+            ],
+            [
+                [1,2,3],
+                [4,5,6],
+                [7,8,9]
+            ]
+        ]
+    ];
+    $scope.data = 'haha';
+})
+.directive('mySudokuCell', function() {
     return {
-        terminal: true,
-        priority: 10,
-        templateUrl: "js/template/mySudokuBox.html"
+        scope: {
+            cellData: "="
+        },
+        templateUrl: "js/template/mySudokuCell.html"
+    }
+})
+.directive('mySudokuBox', function() {
+    return {
+        scope: {
+            boxData: "="
+        },
+        templateUrl: "js/template/mySudokuBox.html",
+        link: function(scope, elem, attrs) {
+            elem.bind('click', function() {
+                console.log(scope);
+            });
+        }
     }
 })
 .directive('mySudoku', function() {
     return {
-        require: 'mySudokubox',
-        transclude: true,
-        templateUrl: "/js/template/mySudoku.html",
-        controller: function($scope) {
-            $scope.data = [
-                [1, 2, 3],
-                [4, 5, 6],
-                [7, 8, 9]
-            ]
-        }
+        templateUrl: "/js/template/mySudoku.html"
     };
 });
